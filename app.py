3040ab1c4b3cacf1a3ae678e142babd8b2842633
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-09 10:53:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,43 @@
 import streamlit as st
 import xgboost as xgb
-import pickle
+import pandas as pd
 
 # Load the data
 @st.cache
 def load_data(file):
-    df = pd.read_csv(file)
-    return df
+    data = pd.read_csv(file)
+    return data
+#data = pd.read_csv("my_data.csv")
 
-# Create a sidebar where users can select the features they want to use
-features = df.columns.tolist()
-selected_features = st.sidebar.multiselect("Select features", options=features)
+# Preprocess the data
+data = data.dropna()
+data = pd.get_dummies(data)
 
-# Create a sidebar where users can enter the hyperparameters for the XGBoost model
-learning_rate = st.sidebar.slider("Learning rate", 0.01, 0.5, 0.1)
-max_depth = st.sidebar.slider("Max depth", 1, 10, 3)
-n_estimators = st.sidebar.slider("Number of estimators", 100, 1000, 500)
+# Split the data into training and test sets
+train_x, test_x, train_y, test_y = train_test_split(data.drop(columns=["target"]), data["target"], test_size=0.2)
 
-# Select the target column
-target_col = st.text_input("Enter the name of the target column:")
+# Train an XGBoost model
+model = xgb.XGBClassifier()
+model.fit(train_x, train_y)
 
-if target_col:
-    if target_col in df.columns:
-        # Split the data into training and test sets
-        X_train, X_test, y_train, y_test = train_test_split(df[selected_features], df[target_col], test_size=0.2)
+# Evaluate the model
+accuracy = model.score(test_x, test_y)
+st.write("Accuracy: ", accuracy)
 
-        # Train the XGBoost model
-        model = xgb.XGBClassifier(learning_rate=learning_rate, max_depth=max_depth, n_estimators=n_estimators)
-        model.fit(X_train, y_train)
+# Use a slider to input custom parameters for the model
+learning_rate = st.slider("Learning rate", 0.01, 0.5, 0.1)
+n_estimators = st.slider("Number of estimators", 10, 500, 100)
+max_depth = st.slider("Max depth", 2, 10, 3)
 
-        # Evaluate the model
-        accuracy = model.score(X_test, y_test)
-        st.write(f"Accuracy: {accuracy:.2f}")
+# Re-train the model with the custom parameters
+model = xgb.XGBClassifier(learning_rate=learning_rate, n_estimators=n_estimators, max_depth=max_depth)
+model.fit(train_x, train_y)
 
-        # Create a Pickle file for the model
-        pickle.dump(model, open("model.pkl", "wb"))
+# Re-evaluate the model with the custom parameters
+accuracy = model.score(test_x, test_y)
+st.write("Accuracy with custom parameters: ", accuracy)
 
-        # Add a button to download the Pickle file
-        if st.button("Download Pickle file"):
-            st.markdown("Model saved as `model.pkl` in the current directory.")
-            st.download("model.pkl")
-    else:
-        st.write("The specified target column is not in the dataframe.")
+# Use a dropdown menu to select a column to make predictions on
+columns = st.multiselect("Select a column", data.columns, default=data.columns[0])
+prediction = model.predict(data[columns])
+st.write("Prediction: ", prediction)
